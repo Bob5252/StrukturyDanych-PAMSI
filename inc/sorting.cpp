286db5993132aedecf42aec788b100f701dac3c2
Bob5252 <79234454+Bob5252@users.noreply.github.com>
2023-04-04 18:54:50 +0200

    Create sorting.cpp

---
+++ inc/sorting.cpp
@@ -0,0 +1,25 @@
+#include "sorting.hpp"
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
